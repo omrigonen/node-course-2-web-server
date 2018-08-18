d7a6dc9c33e932712e473e4bc778574a7ba98b99
Omri Gonen <omri.gonen@gmail.com>
2018-08-18 18:32:04 +0300

    add project page

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about',(req, res) =>{
     });
 });
 
+app.get('/projects',(req, res) =>{
+    res.render('projects.hbs',{
+        pageTitle: 'Protfolio Page',
+    });
+});
+
 app.get('/bad',(req, res) =>{
     res.send({
         errorMessage: 'unable to connect to API Server'
